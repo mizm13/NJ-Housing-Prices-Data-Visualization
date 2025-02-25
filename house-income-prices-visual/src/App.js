e09460c0121f51066be91d71d1aaae2a59622ea0
mizm13 <mizanmodak@gmail.com>
2025-02-25 15:47:45 -0500

    updated App.js to render compoennts

--- house-income-prices-visual/src/App.js
+++ house-income-prices-visual/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 //import './App.css';
 import LineChart from './LineChart';
+import Legend from './Legend';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
       <LineChart />
+      <Legend /> {/* Legend is now separate from the chart */}
     </div>
   );
 }
