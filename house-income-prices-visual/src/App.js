f5666bfab42b970db126bfd012b56ef9c4a252b8
mizm13 <mizanmodak@gmail.com>
2025-03-14 15:23:07 -0400

    Updated background color

--- house-income-prices-visual/src/App.js
+++ house-income-prices-visual/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 //import './App.css';
-import LineChart from './LineChart';
-import Legend from './Legend';
-import BarChart from './BarChart';
 import Dashboard from './Dashboard';
+//import Navbar from './Navbar';
 
 function App() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
+    <div style={{ backgroundColor: '#EAEAEA', display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
+      
       <Dashboard/>
     </div>
   );
