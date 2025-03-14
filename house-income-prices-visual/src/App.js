ee63fe708127fc525612f790c0391bd708ac7417
mizm13 <mizanmodak@gmail.com>
2025-03-14 02:03:11 -0400

    Updated App.js to use Dashboard component

--- house-income-prices-visual/src/App.js
+++ house-income-prices-visual/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 //import './App.css';
 import LineChart from './LineChart';
 import Legend from './Legend';
+import BarChart from './BarChart';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
-      <LineChart />
-      <Legend /> {/* Legend is now separate from the chart */}
+      <Dashboard/>
     </div>
   );
 }
+
 
 export default App;
